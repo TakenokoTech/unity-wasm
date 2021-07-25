c9fa4a07e4f1634db4ff5ecf7253323633a8bf06
takenoko <takenoko.tech@gmail.com>
2021-07-26 00:52:29 +0900

    fix scene.

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField] private Canvas webCanvas;
+    [SerializeField] private Canvas mobileCanvas;
+    
+    void Update()
+    {
+        webCanvas.gameObject.SetActive(Screen.width > Screen.height);
+        mobileCanvas.gameObject.SetActive(Screen.width <= Screen.height);
+    }
+}
